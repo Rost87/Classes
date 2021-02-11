141a7b4d63847e79ecb5638c8196e8786cc5b507
Rost87 <34788968+Rost87@users.noreply.github.com>
2021-02-11 22:11:53 +0500

    Добавил код

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,27 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        String answer;
+
+
+
+
+
+        try (Scanner scan = new Scanner(System.in)) {
+
+            for (int i = 0; i <3; i++) {
+                answer = scan.nextLine();//scan.nextInt();
+                System.out.println("Ответ: " + answer);
+            }
+        } catch (Exception e) {
+            System.out.println("Нужно выбрать цифру, соответствующую варианту ответа");
+        }
     }
+
+
 }
--- src/com/company/SystemTestMain.java
+++ src/com/company/SystemTestMain.java
@@ -3,87 +3,129 @@ package com.company;
 import java.io.*;
 import java.util.*;
 
+
 public class SystemTestMain {
-    static ArrayList<Question> readQuestions(){
+    static ArrayList<Question> readQuestions(String courseName) {
         ArrayList<Question> questions = new ArrayList<>();
 
-        try(BufferedReader br = new BufferedReader(new FileReader("java.txt")))
-        {
-           String name;
-           while ((name = br.readLine()) != null){
-               ArrayList<String> answers = new ArrayList<>();
-               for (int i = 0; i < 4; i++) {
-                   String answer = br.readLine();
-                   answers.add(answer);
-               }
-               int rightAnswer = Integer.parseInt(br.readLine());
-               Question question = new Question(name, answers, rightAnswer);
-               questions.add(question);
-           }
-        }
-        catch (IOException e){
+        try (BufferedReader br = new BufferedReader(new FileReader("java.txt"))) {
+            String name;
+            while ((name = br.readLine()) != null) {
+                ArrayList<String> answers = new ArrayList<>();
+                for (int i = 0; i < 4; i++) {
+                    String answer = br.readLine();
+                    answers.add(answer);
+                }
+                int rightAnswer = Integer.parseInt(br.readLine());
+                Question question = new Question(name, answers, rightAnswer);
+                questions.add(question);
+            }
+        } catch (IOException e) {
 
         }
         return questions;
     }
 
+
+    //ф-ия для вывода вопросов и записи ответов пользователя
+    static HashMap<Integer, Integer> outputQuestion(ArrayList<Question> questionsList) {
+        HashMap<Integer, Integer> questionAnswer = new HashMap<>();
+        try (Scanner scan = new Scanner(System.in)) {
+            for (int i = 0; i < questionsList.size(); i++) {
+                ArrayList<String> questions = questionsList.get(i).getAnswers();
+                System.out.println(questionsList.get(i).getName());
+                for (int j = 0; j < questions.size(); j++) {
+                    System.out.println((j+1) + ". "+ questions.get(j));
+                }
+                System.out.println("Выберите вариант ответа: ");
+                int answer;
+                answer = scan.nextInt();
+                questionAnswer.put(i + 1, answer);
+            }
+        } catch (Exception e) {
+            System.out.println("Нужно выбрать цифру, соответствующую варианту ответа");
+        }
+        return questionAnswer;
+    }
+
+    static String estimate(ArrayList<Question> questionsList, HashMap<Integer, Integer> questionAnswer) {
+        int rightAnswerNumber = 0;
+
+        for (int i = 0; i < questionsList.size(); i++) {
+            int rightAnswer = questionsList.get(i).getRightAnswer();
+            if (rightAnswer == questionAnswer.get(i + 1)) {
+                rightAnswerNumber++;
+            }
+        }
+        double average = rightAnswerNumber / questionsList.size();
+
+        if (average >= 0.8) {
+            return "Курс успешно пройден";
+        } else {
+            return "Курс не пройден";
+        }
+
+
+    }
+
+
     //ф-ия для считывания из файла логинов и паролей всех пользователей
-    static HashMap<String, String> loginPasswordInfo(){
+    static HashMap<String, String> loginPasswordInfo() {
         HashMap<String, String> loginPassword = new HashMap<>();
-        try(BufferedReader br = new BufferedReader(new FileReader("./PersonInfo/LoginPassword.txt") )){
+        try (BufferedReader br = new BufferedReader(new FileReader("./PersonInfo/LoginPassword.txt"))) {
             String line;
             String[] personInfo;
             String login;
             String password;
-            while ((line = br.readLine()) != null){
+            while ((line = br.readLine()) != null) {
                 personInfo = line.split(" ");
                 login = personInfo[0];
                 password = personInfo[1];
                 loginPassword.put(login, password);
             }
-        }catch(IOException e){
+        } catch (IOException e) {
             System.out.println("Ошибка");
         }
         return loginPassword;
     }
 
     //ф-ия для проверки введенного логина и пароля
-    static boolean checkLoginPassword(HashMap<String, String> loginPassword, String login, String password){
+    static boolean checkLoginPassword(HashMap<String, String> loginPassword, String login, String password) {
         String result;
-        if(loginPassword.containsKey(login)){
+        if (loginPassword.containsKey(login)) {
             result = loginPassword.getOrDefault(login, "Неверный пароль");
-        }else{
-           result = "Неверный логин";
+        } else {
+            result = "Неверный логин";
         }
-        return (!result.equals("Неверный пароль"))&&(!result.equals("Неверный логин"));
+        return (!result.equals("Неверный пароль")) && (!result.equals("Неверный логин"));
     }
 
     //ф-ия для вывода всех курсов с вопросами
-    static String[] courseList(){
+    static HashMap<String, String> courseList() {
         File path = new File("./Courses");
         String[] list;
         list = path.list();
-        removeExtension(list);
+        HashMap<String, String> extendedNameList = new HashMap<>();
+        removeExtension(list, extendedNameList);
         Arrays.sort(list, String.CASE_INSENSITIVE_ORDER);
-        return list;
+
+
+        return extendedNameList;
     }
 
-    static void removeExtension(String[] list){
+    static void removeExtension(String[] list, HashMap<String, String> extendedNameList) {
         for (int i = 0; i < list.length; i++) {
             int pointIndex = list[i].lastIndexOf('.');
+            String fullCourseName = list[i];
             list[i] = list[i].substring(0, pointIndex);
+            extendedNameList.put(list[i], fullCourseName);
         }
 
     }
-
 
 
     public static void main(String[] args) {
 
-
-    //    ArrayList<Question> questions = readQuestions();
-    //    System.out.println(questions);
-       // Collections.shuffle(questions);
         //запрос логина и пароля при входе в систему
         Scanner scan = new Scanner(System.in);
         System.out.println("Для входа в систему введите логин: ");
@@ -93,30 +135,31 @@ public class SystemTestMain {
         HashMap<String, String> loginPassword = loginPasswordInfo();
         boolean check = checkLoginPassword(loginPassword, login, password);
 
-        if(check){
-          String[] courseList = courseList();
-            for (String courseName : courseList) {
-                System.out.println(courseName);
+        if (check) {
+            HashMap<String, String> courseList = courseList();
+            for (Map.Entry<String, String> pair : courseList.entrySet()) {
+                System.out.println(pair.getKey());
             }
 
+            System.out.println("Выберите курс, по которому вы хотите пройти тест: ");
+            String courseName = scan.nextLine();
+            HashMap<Integer, Integer> questionAnswer;// = new HashMap<>();
+            if (courseList.containsKey(courseName)) {
+                ArrayList<Question> questions = readQuestions(courseName);
+                questionAnswer = outputQuestion(questions);
+                String result = estimate(questions, questionAnswer);
+                System.out.println(result);
 
+            } else {
+                System.out.println("Вы выбрали несуществующий курс");
+            }
 
-        } else{
+        } else {
             System.out.println("Указаны неверный логин или пароль");
         }
 
 
 
-
-
-
-
-      //  System.out.println(loginPassword);
-
-
-
-        // вывод названий всех доступных тестов
-        //dirList();
 
     }
 }
