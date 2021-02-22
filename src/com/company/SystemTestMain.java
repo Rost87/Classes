a5978f6771dc38611696f2051099af61a0e0dc1d
Rost87 <34788968+Rost87@users.noreply.github.com>
2021-02-22 11:34:08 +0500

    Добавлена новая ф-ия: проверка даты прохождения курсов

--- src/com/company/SystemTestMain.java
+++ src/com/company/SystemTestMain.java
@@ -5,10 +5,12 @@ import java.util.*;
 
 
 public class SystemTestMain {
-    static ArrayList<Question> readQuestions(String courseName) {
-        ArrayList<Question> questions = new ArrayList<>();
 
-        try (BufferedReader br = new BufferedReader(new FileReader("java.txt"))) {
+
+    static ArrayList<Question> readQuestions(String courseName, String pathName) {
+        ArrayList<Question> questions = new ArrayList<>();
+        String fullName = pathName + courseName;
+        try (BufferedReader br = new BufferedReader(new FileReader(fullName))) {
             String name;
             while ((name = br.readLine()) != null) {
                 ArrayList<String> answers = new ArrayList<>();
@@ -26,16 +28,16 @@ public class SystemTestMain {
         return questions;
     }
 
-
     //ф-ия для вывода вопросов и записи ответов пользователя
     static HashMap<Integer, Integer> outputQuestion(ArrayList<Question> questionsList) {
         HashMap<Integer, Integer> questionAnswer = new HashMap<>();
         try (Scanner scan = new Scanner(System.in)) {
             for (int i = 0; i < questionsList.size(); i++) {
-                ArrayList<String> questions = questionsList.get(i).getAnswers();
-                System.out.println(questionsList.get(i).getName());
-                for (int j = 0; j < questions.size(); j++) {
-                    System.out.println((j+1) + ". "+ questions.get(j));
+                ArrayList<String> answers = questionsList.get(i).getAnswers();
+                String question = questionsList.get(i).getName();
+                System.out.println(question);
+                for (int j = 0; j < answers.size(); j++) {
+                    System.out.println(answers.get(j));
                 }
                 System.out.println("Выберите вариант ответа: ");
                 int answer;
@@ -48,7 +50,8 @@ public class SystemTestMain {
         return questionAnswer;
     }
 
-    static String estimate(ArrayList<Question> questionsList, HashMap<Integer, Integer> questionAnswer) {
+    //возвращаем процент правильных ответов
+    static double percentageRightAnswer(ArrayList<Question> questionsList, HashMap<Integer, Integer> questionAnswer) {
         int rightAnswerNumber = 0;
 
         for (int i = 0; i < questionsList.size(); i++) {
@@ -57,22 +60,28 @@ public class SystemTestMain {
                 rightAnswerNumber++;
             }
         }
-        double average = rightAnswerNumber / questionsList.size();
+        double averagePercent = (double) rightAnswerNumber / questionsList.size();
 
-        if (average >= 0.8) {
-            return "Курс успешно пройден";
-        } else {
-            return "Курс не пройден";
+        return Math.round(averagePercent*100);
+    }
+
+    static int estimate(double percentRightAnswer){
+        int estimate = 2;
+        if (percentRightAnswer > 80){
+            estimate = 5;
+        } else if (percentRightAnswer > 60 && percentRightAnswer < 80){
+            estimate = 4;
+        } else if (percentRightAnswer > 40 && percentRightAnswer < 60){
+            estimate = 3;
         }
-
-
+        return estimate;
     }
 
 
     //ф-ия для считывания из файла логинов и паролей всех пользователей
-    static HashMap<String, String> loginPasswordInfo() {
+    static HashMap<String, String> loginPasswordInfo(String pathLoginPassword) {
         HashMap<String, String> loginPassword = new HashMap<>();
-        try (BufferedReader br = new BufferedReader(new FileReader("./PersonInfo/LoginPassword.txt"))) {
+        try (BufferedReader br = new BufferedReader(new FileReader(pathLoginPassword))) {
             String line;
             String[] personInfo;
             String login;
@@ -84,41 +93,78 @@ public class SystemTestMain {
                 loginPassword.put(login, password);
             }
         } catch (IOException e) {
-            System.out.println("Ошибка");
+            System.out.println(e);
         }
         return loginPassword;
     }
 
     //ф-ия для проверки введенного логина и пароля
     static boolean checkLoginPassword(HashMap<String, String> loginPassword, String login, String password) {
-        String result;
-        if (loginPassword.containsKey(login)) {
-            result = loginPassword.getOrDefault(login, "Неверный пароль");
-        } else {
-            result = "Неверный логин";
-        }
-        return (!result.equals("Неверный пароль")) && (!result.equals("Неверный логин"));
+
+        return loginPassword.containsKey(login) && loginPassword.get(login).equals(password);
     }
 
     //ф-ия для вывода всех курсов с вопросами
-    static HashMap<String, String> courseList() {
-        File path = new File("./Courses");
+    static HashMap<String, String> courseList(String pathName) {
+        File path = new File(pathName);
         String[] list;
         list = path.list();
-        HashMap<String, String> extendedNameList = new HashMap<>();
-        removeExtension(list, extendedNameList);
+        HashMap<String, String> extendedNameList;
+        extendedNameList = removeExtension(list);
         Arrays.sort(list, String.CASE_INSENSITIVE_ORDER);
-
 
         return extendedNameList;
     }
 
-    static void removeExtension(String[] list, HashMap<String, String> extendedNameList) {
+    static HashMap<String, String>  removeExtension(String[] list) {
+        HashMap<String, String> extendedNameList = new HashMap<>();
         for (int i = 0; i < list.length; i++) {
             int pointIndex = list[i].lastIndexOf('.');
             String fullCourseName = list[i];
             list[i] = list[i].substring(0, pointIndex);
             extendedNameList.put(list[i], fullCourseName);
+        }
+        return extendedNameList;
+    }
+
+    // ф-ия, возвращающая true, если последний раз курс был пройден пользователем более 24 ч назад
+    static boolean checkCourseDate(String pathLoginCourseDate, String personLogin, String personCourseName, long personMillis){
+        File file = new File(pathLoginCourseDate);
+
+        try{
+            // если файл создан
+            if(!file.createNewFile()){
+                BufferedReader br = new BufferedReader(new FileReader(pathLoginCourseDate));
+                String line, login, courseName;
+                long millisec = 0;
+                String[] info;
+                while((line = br.readLine()) != null){
+                    info = line.split(" ");
+                    login = info[0];
+                    courseName = info[1];
+                    if (personLogin.equals(login) && personCourseName.equals(courseName))
+                        millisec = Long.parseLong(info[2]);
+                }
+                if((personMillis - millisec) > 24*60*60*1000)
+                    return true;
+            } else{
+                  return true;
+            }
+        }catch(Exception e){
+            System.out.println(e);
+        }
+
+       return false;
+    }
+
+    // ф-ия для записи информации о дате прохождения курса
+    static void writeCourseDate(String pathLoginCourseDate, String personLogin, String personCourseName, long personMillis){
+        File file = new File(pathLoginCourseDate);
+        // использую конструктор, для записи в конец файла
+        try(FileWriter writer = new FileWriter(file, true);){
+            writer.append(personLogin + " " + personCourseName + " " + personMillis + "\n");
+        }catch(Exception e){
+            System.out.println(e);
         }
 
     }
@@ -126,30 +172,52 @@ public class SystemTestMain {
 
     public static void main(String[] args) {
 
+        // путь к папке, где хранятся курсы
+        String pathCourse = "./Courses/";
+
+        // путь к файлу, где хранятся данные о времени прохождения курсов каждого из пользователей
+        String pathLoginCourseDate = "./PersonInfo/LoginCourseDate.txt";
+
+        // путь к файлу, где хранятся данные о логинах и паролях пользователей
+        String pathLoginPassword = "./PersonInfo/LoginPassword.txt";
+
         //запрос логина и пароля при входе в систему
         Scanner scan = new Scanner(System.in);
         System.out.println("Для входа в систему введите логин: ");
         String login = scan.nextLine();
         System.out.println("Введите пароль: ");
         String password = scan.nextLine();
-        HashMap<String, String> loginPassword = loginPasswordInfo();
+        HashMap<String, String> loginPassword = loginPasswordInfo(pathLoginPassword);
         boolean check = checkLoginPassword(loginPassword, login, password);
 
         if (check) {
-            HashMap<String, String> courseList = courseList();
+            HashMap<String, String> courseList = courseList(pathCourse);
             for (Map.Entry<String, String> pair : courseList.entrySet()) {
                 System.out.println(pair.getKey());
             }
 
             System.out.println("Выберите курс, по которому вы хотите пройти тест: ");
             String courseName = scan.nextLine();
-            HashMap<Integer, Integer> questionAnswer;// = new HashMap<>();
+            HashMap<Integer, Integer> questionAnswer;
             if (courseList.containsKey(courseName)) {
-                ArrayList<Question> questions = readQuestions(courseName);
-                questionAnswer = outputQuestion(questions);
-                String result = estimate(questions, questionAnswer);
-                System.out.println(result);
+                Calendar date = new GregorianCalendar();
+                long timeNow = date.getTimeInMillis();
+                //проверка даты прохождения курсов
+                if (checkCourseDate(pathLoginCourseDate, login, courseName, timeNow)){
+                    ArrayList<Question> questions = readQuestions(courseList.get(courseName), pathCourse);
+                    questionAnswer = outputQuestion(questions);
+                    // процент правильных ответов
+                    double percentRightAnswer = percentageRightAnswer(questions, questionAnswer);
+                    // оценка по 5-ти бальной шкале
+                    int estimate = estimate(percentRightAnswer);
+                    System.out.println("Процент правильных ответов составил: " + percentRightAnswer);
+                    System.out.println("Ваша оценка (по 5-ти бальной шкале): " + estimate);
+                    writeCourseDate(pathLoginCourseDate, login, courseName, date.getTimeInMillis());
 
+                } else{
+                    System.out.println("С момента последнего прохождения курса прощло менее 24 ч");
+
+                }
             } else {
                 System.out.println("Вы выбрали несуществующий курс");
             }
@@ -157,10 +225,6 @@ public class SystemTestMain {
         } else {
             System.out.println("Указаны неверный логин или пароль");
         }
-
-
-
-
     }
 }
 
