830f6f2afa30950628fe5325f212cae80698e192
Rost87 <34788968+Rost87@users.noreply.github.com>
2021-02-24 21:57:37 +0500

    Добавлены новые методы

--- src/com/company/UserLinkedList.java
+++ src/com/company/UserLinkedList.java
@@ -3,13 +3,12 @@ package com.company;
 import java.util.LinkedList;
 
 public class UserLinkedList {
-    public static class Node
-    {
+    public static class Node {
         private int data;
         private Node next;
         private Node pred;
 
-        public Node(int data){
+        public Node(int data) {
             this.data = data;
         }
     }
@@ -21,10 +20,9 @@ public class UserLinkedList {
     public void addLast(int item) {
         Node p = new Node(item);
 
-        if (head == null){
+        if (head == null) {
             head = tail = p;
-        }
-        else {
+        } else {
             tail.next = p;
             p.pred = tail;
             tail = p;
@@ -36,10 +34,10 @@ public class UserLinkedList {
         return count;
     }
 
-    public int maxElement(){
+    public int maxElement() {
         Node p = head;
         int max = p.data;
-        while (p != null){
+        while (p != null) {
             if (p.data > max)
                 max = p.data;
             p = p.next;
@@ -47,7 +45,7 @@ public class UserLinkedList {
         return max;
     }
 
-    public int get(int index){
+    public int get(int index) {
         if (index < 0 || index >= count)
             throw new IndexOutOfBoundsException();
 
@@ -55,28 +53,28 @@ public class UserLinkedList {
             Node p = head;
             return p.data;
         }
-        if (index < count / 2){
+        if (index < count / 2) {
             Node p = head;
             for (int i = 0; i < index; i++) {
                 p = p.next;
             }
             return p.data;
-        }else {
+        } else {
             Node p = tail;
 
-            for (int i = 0; i < count - index; i++) {
+            for (int i = 0; i < count - index - 1; i++) {
                 p = p.pred;
             }
             return p.data;
         }
     }
 
-    public void addFirst(int elem){
+    public void addFirst(int elem) {
         Node p = new Node(elem);
-        if (head == null){
+        if (head == null) {
             head = tail = p;
 
-        } else{
+        } else {
             head.pred = p;
             p.next = head;
             head = p;
@@ -84,6 +82,166 @@ public class UserLinkedList {
         count++;
     }
 
+    public boolean insert(int elem, int index) {
+        Node pNew = new Node(elem);
+
+        if (index < 0 || index > count)
+            throw new IndexOutOfBoundsException();
+
+        if (index == count) {
+            addLast(elem);
+            return true;
+        }
+
+        if (index == 0) {
+            addFirst(elem);
+            return true;
+        }
+
+        Node pAfter, pBefore;
+        if (index < count / 2) {
+            Node p = head;
+            for (int i = 0; i < index; i++) {
+                p = p.next;
+            }
+            pAfter = p;
+            pBefore = pAfter.pred;
+
+        } else {
+            Node p = tail;
+            for (int i = 0; i < count - index; i++) {
+                p = p.pred;
+            }
+            pBefore = p;
+            pAfter = pBefore.next;
+
+        }
+
+        pNew.next = pAfter;
+        pNew.pred = pBefore;
+        pAfter.pred = pNew;
+        pBefore.next = pNew;
+        count++;
+
+        return true;
+
+    }
+
+    public boolean removeAt(int index) {
+        if (index < 0 || index >= count)
+            throw new IndexOutOfBoundsException();
+
+        Node p, pBefore, pAfter;
+
+        if (index == 0){
+            head = head.next;
+            head.pred = null;
+            count--;
+            return true;
+
+        }
+
+        if (index == count - 1){
+            tail = tail.pred;
+            tail.next = null;
+            count--;
+            return true;
+        }
+
+        if (index < count / 2) {
+            p = head;
+            for (int i = 0; i < index; i++) {
+                p = p.next;
+            }
+        } else {
+            p = tail;
+            for (int i = 0; i < count - index; i++) {
+                p = p.pred;
+            }
+        }
+        pAfter = p.next;
+        pBefore = p.pred;
+        pAfter.pred = pBefore;
+        pBefore.next = pAfter;
+        p.next = p.pred = null;
+        count--;
+
+        return true;
+
+    }
+
+    public boolean removeFirstEntry(int elem){
+
+        if (count == 0)
+            return false;
+
+        Node p = head;
+        int i = 0;
+        do{
+            if(p.data == elem){
+                removeAt(i);
+                return true;
+            }
+            i++;
+        }while((p = p.next) != null);
+
+        return false;
+    }
+
+
+    // не работает
+    public boolean removeAll(int elem){
+        if (count == 0)
+            return false;
+
+        Node p = head;
+        Node pSave = p;
+        // используется для удаления элемента по его индексу
+        int i = 0;
+        // counter = 0, если не было ни одного удаления
+        int counter = 0;
+        do{
+            pSave = pSave.next;
+            if(p.data == elem){
+                removeAt(i);
+                counter++;
+                i--;
+            }
+            p = pSave;
+            i++;
+        }while(pSave != null);
+
+       return counter != 0;
+    }
+
+
+    public boolean removeFirst(){
+        if (count == 0)
+            return false;
+
+        if(count == 1){
+            head = tail = null;
+        }else{
+            head = head.next;
+            head.pred = null;
+        }
+        count--;
+        return true;
+    }
+
+    public boolean removeLast(){
+        if (count == 0)
+            return false;
+
+        if(count == 1){
+            head = tail = null;
+        }else{
+            tail = tail.pred;
+            tail.next = null;
+        }
+        count--;
+        return true;
+    }
 
 }
 
--- src/com/company/UserLinkedListTest.java
+++ src/com/company/UserLinkedListTest.java
@@ -21,13 +21,13 @@ public class UserLinkedListTest {
     @Test
     public void getEndElementTest(){
         UserLinkedList uLL = new UserLinkedList();
-        for (int i = 3; i < 15; i++) {
+        for (int i = 3; i < 8; i++) {
             uLL.addLast(i);
         }
 
-        int expectedValue = 12;
+        int expectedValue = 7;
 
-        assertEquals(expectedValue, uLL.get(10));
+        assertEquals(expectedValue, uLL.get(4));
     }
 
     @Test
@@ -86,5 +86,288 @@ public class UserLinkedListTest {
 
     }
 
+    @Test
+    public void insertInMiddleTest(){
+        UserLinkedList uLL = new UserLinkedList();
+
+        for (int i = 3; i < 9; i++) {
+            uLL.addLast(i);
+        }
+        uLL.insert(11, 2);
+
+        int expectedValue = 11;
+        int expectedCount = 7;
+
+        assertEquals(expectedValue, uLL.get(2));
+        assertEquals(expectedCount, uLL.getCount());
+
+    }
+
+
+    @Test
+    public void insertInMiddleTest2(){
+        UserLinkedList uLL = new UserLinkedList();
+
+        for (int i = 3; i < 9; i++) {
+            uLL.addLast(i);
+        }
+        uLL.insert(11, 5);
+
+        int expectedValue = 11;
+        int expectedCount = 7;
+
+        assertEquals(expectedValue, uLL.get(5));
+        assertEquals(expectedCount, uLL.getCount());
+
+    }
+
+    @Test
+    public void insertInBeginTest(){
+        UserLinkedList uLL = new UserLinkedList();
+
+        for (int i = 3; i < 9; i++) {
+            uLL.addLast(i);
+        }
+        uLL.insert(11, 0);
+
+        int expectedValue = 11;
+        int expectedCount = 7;
+
+        assertEquals(expectedValue, uLL.get(0));
+        assertEquals(expectedCount, uLL.getCount());
+
+    }
+
+    @Test
+    public void insertInEndTest(){
+        UserLinkedList uLL = new UserLinkedList();
+
+        for (int i = 3; i < 9; i++) {
+            uLL.addLast(i);
+        }
+        uLL.insert(11, 6);
+
+        int expectedValue = 11;
+        int expectedCount = 7;
+
+        assertEquals(expectedValue, uLL.get(6));
+        assertEquals(expectedCount, uLL.getCount());
+
+    }
+
+    @Test
+    public void removeAtMiddleTest(){
+        UserLinkedList uLL = new UserLinkedList();
+
+        for (int i = 3; i < 9; i++) {
+            uLL.addLast(i);
+        }
+        uLL.removeAt(1);
+
+        int expectedValue = 5;
+        int expectedCount = 5;
+
+        assertEquals(expectedValue, uLL.get(1));
+        assertEquals(expectedCount, uLL.getCount());
+
+    }
+
+    @Test
+    public void removeAtFirstElemTest(){
+        UserLinkedList uLL = new UserLinkedList();
+
+        for (int i = 3; i < 9; i++) {
+            uLL.addLast(i);
+        }
+        uLL.removeAt(0);
+
+        int expectedValue = 4;
+        int expectedCount = 5;
+
+        assertEquals(expectedValue, uLL.get(0));
+        assertEquals(expectedCount, uLL.getCount());
+
+    }
+
+    @Test
+    public void removeAtLastElemTest(){
+        UserLinkedList uLL = new UserLinkedList();
+
+        for (int i = 3; i < 9; i++) {
+            uLL.addLast(i);
+        }
+        uLL.removeAt(5);
+
+        int expectedValue = 7;
+        int expectedCount = 5;
+
+        assertEquals(expectedValue, uLL.get(4));
+        assertEquals(expectedCount, uLL.getCount());
+
+    }
+
+    @Test // в списке есть повторяющиеся значения
+    public void removeFirstEntryTest2(){
+        UserLinkedList uLL = new UserLinkedList();
+
+        for (int i = 3; i < 9; i++) {
+            uLL.addLast(i);
+        }
+        uLL.addLast(4);
+        uLL.removeFirstEntry(4);
+
+        int expectedValue = 5;
+        int expectedCount = 6;
+
+        assertEquals(expectedValue, uLL.get(1));
+        assertEquals(expectedCount, uLL.getCount());
+
+    }
+
+    @Test
+    public void removeFirstEntryInMiddleTest(){
+        UserLinkedList uLL = new UserLinkedList();
+
+        for (int i = 3; i < 9; i++) {
+            uLL.addLast(i);
+        }
+        uLL.removeFirstEntry(4);
+
+        int expectedValue = 5;
+        int expectedCount = 5;
+
+        assertEquals(expectedValue, uLL.get(1));
+        assertEquals(expectedCount, uLL.getCount());
+
+    }
+
+
+    @Test
+    public void removeFirstEntryInBeginTest(){
+        UserLinkedList uLL = new UserLinkedList();
+
+        for (int i = 3; i < 9; i++) {
+            uLL.addLast(i);
+        }
+        uLL.removeFirstEntry(3);
+
+        int expectedValue = 4;
+        int expectedCount = 5;
+
+        assertEquals(expectedValue, uLL.get(0));
+        assertEquals(expectedCount, uLL.getCount());
+
+    }
+
+    @Test
+    public void removeFirstEntryInEndTest(){
+        UserLinkedList uLL = new UserLinkedList();
+
+        for (int i = 3; i < 9; i++) {
+            uLL.addLast(i);
+        }
+        uLL.removeFirstEntry(8);
+
+        int expectedValue = 7;
+        int expectedCount = 5;
+
+        assertEquals(expectedValue, uLL.get(4));
+        assertEquals(expectedCount, uLL.getCount());
+
+    }
+
+    @Test
+    public void removeAllTest(){
+        UserLinkedList uLL = new UserLinkedList();
+
+        for (int i = 3; i < 9; i++) {
+            uLL.addLast(i);
+        }
+
+        uLL.removeAll(4);
+
+        int expectedValue = 5;
+        int expectedCount = 5;
+
+        assertEquals(expectedValue, uLL.get(1));
+        assertEquals(expectedCount, uLL.getCount());
+
+    }
+
+    @Test
+    public void removeAllSeveralElemTest(){
+        UserLinkedList uLL = new UserLinkedList();
+
+        for (int i = 3; i < 9; i++) {
+            uLL.addLast(i);
+        }
+        uLL.addLast(3);
+        uLL.addLast(11);
+        uLL.addLast(4);
+
+        uLL.removeAll(4);
+
+        int expectedValue1 = 5;
+        int expectedValue2 = 11;
+        int expectedCount = 7;
+
+        assertEquals(expectedValue1, uLL.get(1));
+        assertEquals(expectedValue2, uLL.get(6));
+        assertEquals(expectedCount, uLL.getCount());
+
+    }
+
+
+    @Test
+    public void removeFirstTest(){
+        UserLinkedList uLL = new UserLinkedList();
+
+        for (int i = 3; i < 9; i++) {
+            uLL.addLast(i);
+        }
+
+        uLL.removeFirst();
+
+        int expectedValue = 4;
+        int expectedCount = 5;
+
+        assertEquals(expectedValue, uLL.get(0));
+        assertEquals(expectedCount, uLL.getCount());
+
+    }
+
+    @Test
+    public void removeFirstTest2(){
+        UserLinkedList uLL = new UserLinkedList();
+
+        uLL.addLast(5);
+
+        uLL.removeFirst();
+
+
+        int expectedCount = 0;
+
+        assertEquals(expectedCount, uLL.getCount());
+
+    }
+
+
+    @Test
+    public void removeLastTest(){
+        UserLinkedList uLL = new UserLinkedList();
+
+        for (int i = 3; i < 9; i++) {
+            uLL.addLast(i);
+        }
+
+        uLL.removeLast();
+
+        int expectedValue = 7;
+        int expectedCount = 5;
+
+        assertEquals(expectedValue, uLL.get(4));
+        assertEquals(expectedCount, uLL.getCount());
+
+    }
 
 }
